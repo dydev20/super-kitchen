119e79e63fb797dcaee4719b385788cd24112dab
Dylan Yuen <dylanyuen22@gmail.com>
2023-01-10 22:42:15 +0000

    Changed styling for Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -12,21 +12,21 @@ export default function Menu(){
 
     return(
         <nav>
-            <div className="flex flex-col relative w-fit z-20 p-2">
+            <div className={menuOpen ?  "flex flex-col fixed w-fit z-20 p-2" : "flex flex-col relative w-fit z-20 p-2"}>
                 {menuOpen ? <GrFormClose size="3rem" onClick={toggleMenuOpen} /> :<BiMenu size="3rem" onClick={toggleMenuOpen}/>}
                 <h2 className="self-center">Menu</h2>
                 
             </div>
             
-            <div className={menuOpen ? "w-[70%] h-screen absolute left-0 top-0" : "hidden"}>
+            <div className={menuOpen ? "w-[70%] h-screen fixed left-0 top-0" : "hidden"}>
                 <ul className="flex flex-col bg-orange-50 ">
-                    <li className="self-center mb-10 font-semibold mt-36">Appetisers</li>
-                    <li className="self-center mb-10 font-semibold">Soups</li>
-                    <li className="self-center mb-10 font-semibold">Curry</li>
-                    <li className="self-center mb-10 font-semibold">Sweet & Sour</li>
-                    <li className="self-center mb-10 font-semibold">Chow Mein</li>
-                    <li className="self-center mb-10 font-semibold">Fried Rice</li>
-                    <li className="self-center mb-10 font-semibold">Sides</li>
+                    <li className="pl-16 mb-10 font-semibold mt-36">Appetisers</li>
+                    <li className="pl-16 mb-10 font-semibold">Soups</li>
+                    <li className="pl-16 mb-10 font-semibold">Curry</li>
+                    <li className="pl-16 mb-10 font-semibold">Sweet & Sour</li>
+                    <li className="pl-16 mb-10 font-semibold">Chow Mein</li>
+                    <li className="pl-16 mb-10 font-semibold">Fried Rice</li>
+                    <li className="pl-16 mb-10 font-semibold">Sides</li>
                 </ul>
             </div>
             
