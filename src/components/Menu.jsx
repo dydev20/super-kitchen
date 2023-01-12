bb524bff90fe4b3b0503765e629bb03b93769dd9
Dylan Yuen <dylanyuen22@gmail.com>
2023-01-12 13:20:35 +0000

    Updated styles

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -12,21 +12,22 @@ export default function Menu(){
 
     return(
         <nav>
-            <div className={menuOpen ?  "flex flex-col fixed w-fit z-20 p-2" : "flex flex-col relative w-fit z-20 p-2"}>
+            <div className={menuOpen ?  "flex flex-col fixed w-fit z-20 p-2 left-0 top-0" : "flex flex-col relative w-fit z-20 p-2"}>
                 {menuOpen ? <GrFormClose size="3rem" onClick={toggleMenuOpen} /> :<BiMenu size="3rem" onClick={toggleMenuOpen}/>}
-                <h2 className="self-center">Menu</h2>
+                <h2 className={menuOpen ? "hidden" : "self-center"}>Menu</h2>
                 
             </div>
             
-            <div className={menuOpen ? "w-[70%] h-screen fixed left-0 top-0" : "hidden"}>
-                <ul className="flex flex-col bg-orange-50 ">
-                    <li className="pl-16 mb-10 font-semibold mt-36">Appetisers</li>
-                    <li className="pl-16 mb-10 font-semibold">Soups</li>
-                    <li className="pl-16 mb-10 font-semibold">Curry</li>
-                    <li className="pl-16 mb-10 font-semibold">Sweet & Sour</li>
-                    <li className="pl-16 mb-10 font-semibold">Chow Mein</li>
-                    <li className="pl-16 mb-10 font-semibold">Fried Rice</li>
-                    <li className="pl-16 mb-10 font-semibold">Sides</li>
+            <div className={menuOpen ? "w-full fixed left-0 top-0" : "hidden"}>
+                <ul className="flex flex-col h-screen justify-around bg-orange-50 ">
+                    <li className="pl-10 font-semibold mt-16">Appetisers</li>
+                    <li className="pl-10 font-semibold">Soups</li>
+                    <li className="pl-10 font-semibold">Curry</li>
+                    <li className="pl-10 font-semibold">Sweet & Sour HK Style</li>
+                    <li className="pl-10 font-semibold">Sweet & Sour Batter Style</li>
+                    <li className="pl-10 font-semibold">Chow Mein</li>
+                    <li className="pl-10 font-semibold">Fried Rice</li>
+                    <li className="pl-10 font-semibold mb-4">Sides</li>
                 </ul>
             </div>
             
