67fcbe9a8895c2cb843fbc7e6742154df6a99554
Dylan Yuen <dylanyuen22@gmail.com>
2023-01-23 17:23:56 +0000

    Fixed bug - PriceDisplay ontop of Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -12,13 +12,13 @@ export default function Menu(){
 
     return(
         <nav>
-            <div className={menuOpen ?  "flex flex-col fixed w-fit z-20 p-2 left-0 top-0" : "flex flex-col relative w-fit z-20 p-2"}>
+            <div className={menuOpen ?  "flex flex-col fixed w-fit z-30 p-2 left-0 top-0" : "flex flex-col relative w-fit z-30 p-2"}>
                 {menuOpen ? <GrFormClose size="3rem" onClick={toggleMenuOpen} /> :<BiMenu size="3rem" onClick={toggleMenuOpen}/>}
                 <h2 className={menuOpen ? "hidden" : "self-center"}>Menu</h2>
                 
             </div>
             
-            <div className={menuOpen ? "w-full fixed left-0 top-0" : "hidden"}>
+            <div className={menuOpen ? "w-full fixed left-0 top-0 z-20" : "hidden"}>
                 <ul className="flex flex-col h-screen justify-around bg-orange-50 ">
                     <li className="pl-10 font-semibold mt-16">Appetisers</li>
                     <li className="pl-10 font-semibold">Soups</li>
