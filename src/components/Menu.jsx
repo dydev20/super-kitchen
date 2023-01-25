a8e8043f5048bcdecfba3fc2bf4b988bb63dbd9a
Dylan Yuen <dylanyuen22@gmail.com>
2023-01-25 14:23:28 +0000

    Added brand name

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -13,37 +13,40 @@ export default function Menu(){
 
     return(
         <nav>
-            <div className={menuOpen ?  "flex flex-col fixed w-fit z-30 p-2 left-0 top-0" : "flex flex-col relative w-fit z-30 p-2"}>
-                {menuOpen ? <GrFormClose size="3rem" onClick={toggleMenuOpen} /> :<BiMenu size="3rem" onClick={toggleMenuOpen}/>}
-                <h2 className={menuOpen ? "hidden" : "self-center"}>Menu</h2>
+            <div className="flex mb-8">
                 
+                <div className={menuOpen ?  "flex flex-col fixed w-fit z-30 p-2 left-0 top-0" : "flex flex-col relative w-fit z-30 p-2"}>
+                    {menuOpen ? <GrFormClose size="2.5rem" onClick={toggleMenuOpen} /> :<BiMenu size="2.5rem" onClick={toggleMenuOpen}/>}
+                    <h2 className={menuOpen ? "hidden" : "self-center text-sm"}>Menu</h2>
+                </div>
+                <h1 className="self-center text-center grow text-2xl text-red-700 font-bold mr-8">Super Kitchen</h1>
             </div>
             
             <div className={menuOpen ? "w-full fixed left-0 top-0 z-20" : "hidden"}>
                 <ul className="flex flex-col h-screen justify-around bg-orange-50 ">
-                    <li className="pl-10 font-semibold mt-16">
+                    <li className="pl-10 font-semibold mt-16 hover:cursor-pointer">
                         <Link to="Appetisers" smooth={true} duration={500}>Appetisers</Link>
                     </li>
-                    <li className="pl-10 font-semibold">
-                        <Link to="Soups" smooth={true} duration={500} offset={55} onClick={toggleMenuOpen}>Soups</Link>
+                    <li className="pl-10 font-semibold hover:cursor-pointer">
+                        <Link to="Soups" smooth={true} duration={500} offset={30} onClick={toggleMenuOpen}>Soups</Link>
                     </li>
-                    <li className="pl-10 font-semibold">
-                        <Link to="Curry" smooth={true} duration={500} offset={55} onClick={toggleMenuOpen}>Curry</Link>
+                    <li className="pl-10 font-semibold hover:cursor-pointer">
+                        <Link to="Curry" smooth={true} duration={500} offset={30} onClick={toggleMenuOpen}>Curry</Link>
                     </li>
-                    <li className="pl-10 font-semibold">
-                        <Link to="Sweet & Sour HK Style" smooth={true} duration={500} offset={55} onClick={toggleMenuOpen}>Sweet & Sour HK Style</Link>
+                    <li className="pl-10 font-semibold hover:cursor-pointer">
+                        <Link to="Sweet & Sour HK Style" smooth={true} duration={500} offset={30} onClick={toggleMenuOpen}>Sweet & Sour HK Style</Link>
                     </li>
-                    <li className="pl-10 font-semibold">
-                        <Link to="Sweet & Sour Batter Style" smooth={true} duration={500} offset={55} onClick={toggleMenuOpen}>Sweet & Sour Batter Style</Link>
+                    <li className="pl-10 font-semibold hover:cursor-pointer">
+                        <Link to="Sweet & Sour Batter Style" smooth={true} duration={500} offset={30} onClick={toggleMenuOpen}>Sweet & Sour Batter Style</Link>
                     </li>
-                    <li className="pl-10 font-semibold">
-                        <Link to="Chow Mein" smooth={true} duration={500} offset={55} onClick={toggleMenuOpen}>Chow Mein</Link>
+                    <li className="pl-10 font-semibold hover:cursor-pointer">
+                        <Link to="Chow Mein" smooth={true} duration={500} offset={30} onClick={toggleMenuOpen}>Chow Mein</Link>
                     </li>
-                    <li className="pl-10 font-semibold">
-                        <Link to="Fried Rice" smooth={true} duration={500} offset={55} onClick={toggleMenuOpen}>Fried Rice</Link>
+                    <li className="pl-10 font-semibold hover:cursor-pointer">
+                        <Link to="Fried Rice" smooth={true} duration={500} offset={30} onClick={toggleMenuOpen}>Fried Rice</Link>
                     </li>
-                    <li className="pl-10 font-semibold mb-4">
-                        <Link to="Sides" smooth={true} duration={500} offset={55} onClick={toggleMenuOpen}>Sides</Link>
+                    <li className="pl-10 font-semibold mb-4 hover:cursor-pointer">
+                        <Link to="Sides" smooth={true} duration={500} offset={30} onClick={toggleMenuOpen}>Sides</Link>
                     </li>
                 </ul>
             </div>
